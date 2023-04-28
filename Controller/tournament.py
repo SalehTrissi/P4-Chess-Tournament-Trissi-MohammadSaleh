de474e7c1660aa8de56b1bf8bdd23c75fcfadc0a
Saleh Trissi <rayatrissi1@gmail.com>
2023-04-28 16:13:29 +0200

    Created def load_tournament who start for select an existing trounement to load et start the tournament

--- Controller/tournament.py
+++ Controller/tournament.py
@@ -1,5 +1,9 @@
+import random
 from datetime import datetime
+
 from texttable import Texttable
+from Model.round import Round
+from Model.tournament import Tournament, TournamentDatabase
 from View.menu import MenuView
 from View.round import RoundViews
 
@@ -35,3 +39,65 @@ class MenuTournamentController:
             table.add_row(row)
 
         print(table.draw().center(100))
+
+    def load_tournament(self):
+        """Select an existing tournament to resume."""
+        # Load the list of existing tournaments from the database
+        tournament_list = TournamentDatabase().load_tournament_list()
+        # Prompt the user to select a tournament
+        self.select_tournament(tournament_list)
+
+        # Loop until a valid tournament ID is entered
+        while True:
+            # Ask the user to enter the ID
+            # of the tournament they want to resume
+            tournament_id = input(
+                "\n- Choose a tournament using the ID number: ")
+
+            # Check if the input is a valid integer
+            if not tournament_id.isdigit():
+                # If not, print an error message and continue the loop
+                print(f"\nInvalid input: {tournament_id}."
+                      f" Please enter a valid integer ID.")
+                continue
+
+            # Loop through the list of tournaments
+            # to find the one with the matching ID
+            for tournament_data in tournament_list:
+                if int(tournament_id) == tournament_data["id"]:
+
+                    # If a matching tournament is found,
+                    # create a Tournament object from its data
+                    tournament = self.create_tournament_from_data(
+                        tournament_data)
+                    # Start the tournament
+                    self.start_tournament(tournament)
+                    # Return from the function
+                    return
+
+            # If no matching tournament is found,
+            # print an error message and continue the loop
+            print(f"\nInvalid input: {tournament_id}."
+                  f" Please choose a valid tournament ID "
+                  f"from the following options:")
+            for tournament_data in tournament_list:
+                # Print a list of all the available tournaments
+                print(f"{tournament_data['id']} - {tournament_data['name']}"
+                      f" ({tournament_data['location']})")
+
+    @staticmethod
+    def create_tournament_from_data(tournament_data):
+        """Create a Tournament object from the given data dictionary."""
+        return Tournament(
+            tournament_data["id"],
+            tournament_data["name"],
+            tournament_data["location"],
+            tournament_data["start_date"],
+            tournament_data["end_date"],
+            tournament_data["description"],
+            tournament_data["nb_current_round"],
+            tournament_data["list_players"],
+            tournament_data["list_rounds"],
+            tournament_data["rounds_total"]
+        )
+    
