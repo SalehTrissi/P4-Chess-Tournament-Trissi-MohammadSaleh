370d2bfe10dad8025c7d0a71da2cabb6d1370c35
Saleh Trissi <rayatrissi1@gmail.com>
2023-04-28 11:33:24 +0200

    Update, I add the commentes for this def return_or_exit

--- Controller/controller.py
+++ Controller/controller.py
@@ -8,20 +8,30 @@ class Controllers:
         self.controller_tournament = MenuTournamentController()
 
     def return_or_exit(self):
+        # A loop that runs indefinitely until 'break' is called.
         while True:
+            # Ask for user input and convert it to lowercase.
             user_input = input(
                 "\nType 'back' to return to main menu, "
                 "'exit' to exit the program: ").lower()
+            # If user types 'back', return to the main menu.
             if user_input == 'back':
                 # Return to the main menu
                 self.controller_tournament.return_to_main_menu()
+                # Exit the loop.
                 break
+            # If user types 'exit', exit the program.
             elif user_input == 'exit':
                 # Exit program
                 """Handles quitting the program"""
+                # Display a goodbye message.
                 self.menu_view.msg_good_bay()
+                # Call a function to exit the program.
                 self.menu_view.exit_program()
+                # Exit the loop.
                 break
+            # If the user input is neither 'back' nor 'exit',
+            # show an error message and ask for input again.
             else:
-                # Show an error message and re-ask for input
+                # Show an error message and re-ask for input.
                 print("Invalid input. Please type 'back' or 'exit'.")
